0fad8429eb59ec79c3bb916a79d5cec7ee9a5a25
treinio <tijnvbek@gmail.com>
2023-04-20 18:05:25 +0200

    Added POST request to register a new user

--- index.js
+++ index.js
@@ -29,6 +29,51 @@ app.use('/', (req, res, next) => {
     next()
 })
 
+// POST request to register a new user (UC-201)
+app.post('/api/user', (req, res) => {
+    let newUser = req.body
+    let { firstName, lastName, street, city, emailAddress, password, phoneNumber } = req.body
+
+    try {
+        assert(typeof firstName === 'string', 'firstName must be a string')
+        assert(typeof lastName === 'string', 'lastName must be a string')
+        assert(typeof street === 'string', 'street must be a string')
+        assert(typeof city === 'string', 'city must be a string')
+        assert(typeof emailAddress === 'string', 'emailAddress must be a string')
+        assert(typeof password === 'string', 'password must be a string')
+        assert(typeof phoneNumber === 'string', 'phoneNumber must be a string')
+
+        index++
+        newUser = {
+            id: index,
+            firstName: firstName,
+            lastName: lastName,
+            street: street,
+            city: city,
+            emailAddress: emailAddress,
+            password: password,
+            phoneNumber: phoneNumber
+        }
+        users.push(newUser)
+        res.status(201).json(
+            {
+                status: 201,
+                message: `User with id ${index} successfully added`,
+                data: newUser
+            }
+        )
+    } 
+    catch (err) {
+        res.status(400).json(
+            {
+                status: 400,
+                message: err.toString(),
+                data: {}
+            }
+        )
+    }
+})
+
 // Listen to http requests
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
