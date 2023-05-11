d90fae620b4a831262c42da1a5595eb160c603f4
TijnVanBekhoven <113771373+TijnVanBekhoven@users.noreply.github.com>
2023-05-11 18:09:49 +0200

    Added mysql github functionality

--- src/util/sql.database.js
+++ src/util/sql.database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shareameal',
-    port: 3306,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    database: process.env.DB_DATABASE || 'shareameal',
+    port: process.env.DB_PORT || 3306,
+    password: process.env.DB_PASSWORD || '',
     multipleStatements: true,
     waitForConnections: true,
     connectionLimit: 10,
